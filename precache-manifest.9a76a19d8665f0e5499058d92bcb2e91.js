dfa37770c77a8fc36298c1e3bf53e94b035a52ea
JohnDarragh <jdarragh79@gmail.com>
2020-08-25 23:29:35 +0100

    Updates

---
+++ precache-manifest.9a76a19d8665f0e5499058d92bcb2e91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9165adf25d355edcfa08e8740781e16f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcfee0ef46dc9432a7ea",
+    "url": "./static/css/main.40d1e5c9.chunk.css"
+  },
+  {
+    "revision": "398cddc1b1f55cba8f23",
+    "url": "./static/js/2.99a2d22d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.99a2d22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfee0ef46dc9432a7ea",
+    "url": "./static/js/main.47237cf3.chunk.js"
+  },
+  {
+    "revision": "5d5c17cc87ac68809c32",
+    "url": "./static/js/runtime-main.42c8a2b9.js"
+  },
+  {
+    "revision": "ce4127c7acf66573fa260278a9dc6b7b",
+    "url": "./static/media/Constitution cover cropped.ce4127c7.png"
+  },
+  {
+    "revision": "565c01742c244f3b47660f30395872d7",
+    "url": "./static/media/bottom_text.565c0174.png"
+  },
+  {
+    "revision": "512f3bc182e8d13b34e57f1265eada22",
+    "url": "./static/media/top_cropped.512f3bc1.png"
+  }
+]);
